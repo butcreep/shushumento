dd939e81212730879c5a415e5125b30fe2d1235e
dysong <daynocreep@gmail.com>
2022-12-25 21:48:58 +0900

    [feature] interface 입력

---
+++ src/types/type.ts
@@ -0,0 +1,20 @@
+export interface ICamp {
+  id: number
+  title: string
+  status: string
+  field: string
+  skill: string
+  startDate: string
+  thumbnail: string
+}
+export interface ICampDetail {
+  id: number
+  title: string
+  name: string
+  status: string
+  field: string
+  skill: string
+  startDate: string
+  process: string
+  thumbnail: string
+}

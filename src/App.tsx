932cdbd32b78c432b1b8162c4246028da7032829
dysong <daynocreep@gmail.com>
2022-12-25 21:49:29 +0900

    [feature] React Router 설정

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import CampDetail from './pages/CampDetail'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/camp/:campId" element={<CampDetail />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App

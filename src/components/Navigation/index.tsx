cddd75011a0c33ec8c446a85dcc2b4a0cb15f285
dysong <daynocreep@gmail.com>
2023-01-01 21:30:50 +0900

    [style] navigation style

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,16 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import profileIcon from "assets/images/icons/img-user-default.png";
 
 const Navigation = () => {
   return (
-    <div>
-      <Link to="/">Shumento</Link>
-      <Link to="/">
-        <img src={profileIcon} alt="" />
-      </Link>
-    </div>
+    <Container>
+      <header>
+        <Link to="/" className="logo">
+          Shumento
+        </Link>
+        <div>
+          <img src={profileIcon} alt="" />
+        </div>
+      </header>
+    </Container>
   );
 };
 
 export default Navigation;
+const Container = styled.header`
+  header {
+    height: 70px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1140px;
+  }
+  .logo {
+    font-size: 20px;
+    font-weight: bold;
+    color: #009688;
+  }
+
+  img {
+    width: 35px;
+  }
+`;

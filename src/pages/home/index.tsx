55dcb8b568382758cf6b48c5c3537fb381f4e803
dysong <daynocreep@gmail.com>
2023-01-01 21:32:10 +0900

    [update] campCard에 title 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,9 @@ import Navigation from "components/Navigation";
 import { getCampsType } from "apis/campApi";
 import { CampType, ICamp } from "types/type";
 import CampCard from "components/CampCard";
+import styled from "styled-components";
+import MainBnr from "./components/MainBnr";
+import Footer from "components/Footer";
 
 const Home = () => {
   const [popularCamps, setPopularCamps] = useState<ICamp[]>();
@@ -24,11 +27,42 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      {popularCamps &&
-        popularCamps.map((camp, index) => <CampCard key={index} camp={camp} />)}
-      {saleCamps &&
-        saleCamps.map((camp, index) => <CampCard key={index} camp={camp} />)}
+      <MainBnr />
+      <Container>
+        <h2 className="title">인기 부트 캠프</h2>
+        <div className="card-section">
+          {popularCamps &&
+            popularCamps.map((camp, index) => (
+              <CampCard key={index} camp={camp} />
+            ))}
+        </div>
+        <h2 className="title">특가 할인 캠프</h2>
+        <div className="card-section">
+          {saleCamps &&
+            saleCamps.map((camp, index) => (
+              <CampCard key={index} camp={camp} />
+            ))}
+        </div>
+      </Container>
+      <Footer />
     </>
   );
 };
 export default Home;
+
+const Container = styled.div`
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 100px 0;
+  .title {
+    font-size: 30px;
+    font-weight: bold;
+    padding-bottom: 30px;
+  }
+  .card-section {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+    padding-bottom: 60px;
+  }
+`;

32e1ba539f3b3c17d10e29c85ee09367383e3f4f
dysong <daynocreep@gmail.com>
2023-01-01 21:31:15 +0900

    [style] campCard styled-components 적용

--- src/components/CampCard/index.tsx
+++ src/components/CampCard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { ICamp } from "../../types/type";
 
 interface IProps {
@@ -8,20 +9,49 @@ interface IProps {
 
 const CampCard = ({ camp }: IProps) => {
   return (
-    <Link to={`/camp/${camp.id}`}>
-      <div>
-        <div>{camp.name}</div>
-        <div>{camp.type}</div>
-        <span>{camp.status}</span>
-        <div>
+    <Container>
+      <Link to={`/camp/${camp.id}`}>
+        <img src={camp.thumbnail} alt={camp.name} />
+
+        <div className="flex_content">
           <span>{camp.field}</span>
           <span>{camp.skill}</span>
+          <span className="status">{camp.status}</span>
         </div>
-        <div>{camp.startDate}</div>
-        <div>{camp.thumbnail}</div>
-      </div>
-    </Link>
+        <div className="camp_name">{camp.name}</div>
+        <div>{camp.startDate} 시작</div>
+      </Link>
+    </Container>
   );
 };
 
 export default CampCard;
+
+const Container = styled.div`
+  .flex_content {
+    display: flex;
+    span {
+      font-size: 12px;
+      line-height: 15px;
+      font-weight: 400;
+      color: rgb(60, 65, 68);
+      margin-right: 5px;
+    }
+    .status {
+      color: rgb(238, 126, 65);
+    }
+  }
+  .camp_name {
+    font-size: 17px;
+    line-height: 20px;
+    color: rgb(4, 5, 5);
+    font-weight: 600;
+    margin: 10px 0;
+  }
+  img {
+    width: 100%;
+    height: 190px;
+    border-radius: 15px;
+    margin-bottom: 15px;
+  }
+`;

75e7c775fdedde737698cd9c5067bb577ec956a5
dysong <daynocreep@gmail.com>
2023-01-02 19:08:56 +0900

    [style] cardDetail header

--- src/pages/CampDetail/index.tsx
+++ src/pages/CampDetail/index.tsx
@@ -22,17 +22,18 @@ const CampDetail = () => {
       <>
         {/* TODO:navigation APP애 공통으로 두고 필요없는 곳에서 뺴는 법 */}
         <Navigation />
+        <Header>
+          <h1>{campDetail.name}</h1>
+          <img src={campDetail.headerImage} alt="" />
+        </Header>
         <Container>
           <div style={{ paddingTop: "60px" }}>
-            <div>
+            <div className="application-box">
               <h1>{campDetail.name}</h1>
-            </div>
-            <div>{campDetail.tags}</div>
-            <div>{campDetail.desc}</div>
-            <div>{campDetail.seat}</div>
-            <div>{campDetail.reviewMaterial}</div>
-            <div>
-              <img src={campDetail.headerImage} alt="" />
+              <div>{campDetail.tags}</div>
+              <div>{campDetail.desc}</div>
+              <div>{campDetail.seat}</div>
+              <div>{campDetail.reviewMaterial}</div>
             </div>
             <div>
               {campDetail.images &&
@@ -57,6 +58,27 @@ const CampDetail = () => {
 };
 
 export default CampDetail;
+const Header = styled.div`
+  background-color: #00bcd4;
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  padding-top: 40px;
+  h1 {
+    font-size: 32px;
+    padding-bottom: 40px;
+    line-height: 40px;
+    color: #ffffff;
+    font-weight: 600;
+    width: 487px;
+    margin-right: 30px;
+  }
+  img {
+    width: 500px;
+    border-radius: 10px 10px 0 0;
+  }
+`;
+
 const Container = styled.div`
   max-width: 1140px;
   margin: 0 auto;
